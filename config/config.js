70b5b263a6f8b74cd38e751224ddfa67d27d8a26
Zafral <amirglenfeliciano18@gmail.com>
2019-12-19 15:30:30 +0800

    second commit

--- config/config.js
+++ config/config.js
@@ -1,13 +1,6 @@
 import firebase from "firebase";
 const config = {
-  apiKey: "AIzaSyBAVe5rpptuF_wfUc4uWlxrqh9Sl544O8Y",
-  authDomain: "clone-eq.firebaseapp.com",
-  databaseURL: "https://clone-eq.firebaseio.com",
-  projectId: "clone-eq",
-  storageBucket: "clone-eq.appspot.com",
-  messagingSenderId: "918890758075",
-  appId: "1:918890758075:web:048fdb724426bc1ea90e9e",
-  measurementId: "G-ZKR3EPR3Z9"
+  //api key
 };
 // Initialize Firebase
 firebase.initializeApp(config);
